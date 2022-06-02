8255820b2108194b4fb4e261d739c0344e7953cd
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-06-01 22:40:00 -0700

    Adding js

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb+srv://root:adminpass@portfoliocluster.nwjaqh0.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true}, { useUnifiedTopology: true });
+
+const dataSchema = {
+    name: String,
+    email: String,
+    subject: String,
+    message: String
+}
+
+const message = mongoose.model("Message", dataSchema);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/styles/index.css');
+})
+
+app.post('/', (req, res) => {
+    let newMessage = new message({
+        name: req.body.Name,
+        email: req.body.Email,
+        subject: req.body.Subject,
+        message: req.body.Message
+    })
+    newMessage.save();
+    res.redirect('/');
+})
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+})
