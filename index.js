cca92f7cf0dfcaa40a0e505e3cd0c7ccb63976b2
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-06-10 12:38:50 -0700

    Updating JS to Make Text Appear Faster

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ TxtType.prototype.tick = function() {
     } else if (this.isDeleting && this.txt === '') {
         this.isDeleting = false;
         this.loopNum++;
-        delta = 500;
+        delta = 100;
     }
 
     setTimeout(function() {
